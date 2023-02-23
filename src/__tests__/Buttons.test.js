e3d70e52955c594322e68e9e3f37ae4c4c5a0d8f
SFarida <faridagueleu@gmail.com>
2023-02-24 00:03:30 +0100

    Fix linter errors

--- src/__tests__/Buttons.test.js
+++ src/__tests__/Buttons.test.js
@@ -1,38 +1,33 @@
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import Buttons from '../components/Buttons';
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-import calculate from '../logic/calculate';
 
 let container = null;
 beforeEach(() => {
-    // setup a DOM element as a render target
-    container = document.createElement("div");
-    document.body.appendChild(container);
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
 });
 
 afterEach(() => {
-    // cleanup on exiting
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
 });
 
 it('Button renders  correctly', () => {
-    const handleClick = (event) => {
-        const values = calculate(state, event.target.innerText);
-        setState(values);
-    };
-    act(() => {
-        render(
-            <Buttons
-                onClick={handleClick}
-            />,
-            container
-        );
-    });
+  const handleClick = () => {};
+  act(() => {
+    render(
+      <Buttons
+        onClick={handleClick}
+      />,
+      container,
+    );
+  });
 
-    expect(
-        container.querySelector("[class='buttons']").childElementCount
-    ).toEqual(19);
+  expect(
+    container.querySelector("[class='buttons']").childElementCount,
+  ).toEqual(19);
 });
-
--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,32 +1,30 @@
-import Calculator from '../components/Calculator'
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-import calculate from '../logic/calculate';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import Calculator from '../components/Calculator';
 
 let container = null;
 beforeEach(() => {
-    // setup a DOM element as a render target
-    container = document.createElement("div");
-    document.body.appendChild(container);
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
 });
 
 afterEach(() => {
-    // cleanup on exiting
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
 });
 
 it('Calculator renders  correctly all buttons', () => {
-    act(() => {
-        render(
-            <Calculator />,
-            container
-        );
-    });
+  act(() => {
+    render(
+      <Calculator />,
+      container,
+    );
+  });
 
-    expect(
-        container.querySelector("[class='buttons']").childElementCount
-    ).toEqual(19);
+  expect(
+    container.querySelector("[class='buttons']").childElementCount,
+  ).toEqual(19);
 });
-
